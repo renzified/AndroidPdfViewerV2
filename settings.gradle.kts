b49e13582032052168ab3df071dca53a87e1a1d5
renz <dev.renzsalanga@gmail.com>
2025-08-22 14:58:45 +0800

    refactor: updated gradle and pdfium dependencies

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+import org.gradle.kotlin.dsl.flatDir
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        flatDir {
+            dirs("libs")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        flatDir {
+            dirs("libs")
+        }
+    }
+}
+
+rootProject.name = "AndroidPdfViewerV2"
+include(":app")
+include(":android-pdf-viewer")
